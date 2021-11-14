a83eed66ce313e08c576a63cc4093b478f141e69
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-11-14 20:40:22 +0100

    Dodaj pliki projektów.

---
+++ BetterSCP.SCP914/PluginHandler.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PluginHandler.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+namespace Mistaken.BetterSCP.SCP914
+{
+    /// <inheritdoc/>
+    internal class PluginHandler : Plugin<Config>
+    {
+        /// <inheritdoc/>
+        public override string Author => "Mistaken Devs";
+
+        /// <inheritdoc/>
+        public override string Name => "BetterSCP SCP079";
+
+        /// <inheritdoc/>
+        public override string Prefix => "MBSCP_079";
+
+        /// <inheritdoc/>
+        public override PluginPriority Priority => PluginPriority.Default;
+
+        /// <inheritdoc/>
+        public override Version RequiredExiledVersion => new Version(3, 6, 2);
+
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+            Instance = this;
+
+            // new Handler(this);
+
+            API.Diagnostics.Module.OnEnable(this);
+
+            base.OnEnabled();
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+            API.Diagnostics.Module.OnDisable(this);
+
+            base.OnDisabled();
+        }
+
+        internal static PluginHandler Instance { get; private set; }
+    }
+}
